77c4e6950a882b4f6a15b5cbb8ed9682239804c6
Nuno Miguel Da Silva Sabino <ist181714@lab6p3.rnl.tecnico.ulisboa.pt>
2016-12-06 19:21:16 +0000

    Changed a line of code that made things look the way they want it to look but they didnt say anything so we had to guess. Ye... thats life bro

--- EntregaFinalYEYEYEYE/IA1617projv06/scripts/SolF3.lisp
+++ EntregaFinalYEYEYEYE/IA1617projv06/scripts/SolF3.lisp
@@ -1,5 +1,5 @@
-(load "datastructures.lisp")
-(load "auxfuncs.lisp")
+(load "datastructures.fas")
+(load "auxfuncs.fas")
 
 
 ;;; TAI position
@@ -69,40 +69,77 @@
     (values new-state)))
 
 ;; Solution of phase 2
-
 ;;; Pedir 
 (defun nextStates (st)
   "generate all possible next states"
-  (let ((possibleStates (list)))
-    (loop for action in (possible-actions)
-      do (setf possibleStates (cons (nextState st action) possibleStates))
-    )
-    possibleStates
-  )
-)
+  (let ((successors nil))
+    (dolist (act (possible-actions) successors)
+      (let ((new-state (nextState st act)))
+	(if (not (member new-state successors :test #'equalp))
+	    (push new-state successors))))))
+
+;;; Solucao e uma seq ordenada de estados
+(defun solution (node)
+  (let ((seq-states nil))
+    (loop 
+      (when (null node)
+	(return))
+      (push (node-state node) seq-states)
+      (setf node (node-parent node)))
+    (values seq-states)))
 
 ;;; limdepthfirstsearch 
-(defun limdepthfirstsearch (problem lim)
-  "limited depth first search"
+(defun limdepthfirstsearch (problem lim &key cutoff?)
+  "limited depth first search
      st - initial state
      problem - problem information
-     lim - depth limit
-	(list (make-node :state (problem-initial-state problem))) )
+     lim - depth limit"
+  (labels ((limdepthfirstsearch-aux (node problem lim)
+	     (if (isGoalp (node-state node))
+		 (solution node)
+		 (if (zerop lim)
+		     :cutoff
+		     (let ((cutoff? nil))
+		       (dolist (new-state (nextStates (node-state node)))
+			 (let* ((new-node (make-node :parent node :state new-state))
+				(res (limdepthfirstsearch-aux new-node problem (1- lim))))
+			   (if (eq res :cutoff)
+			       (setf cutoff? :cutoff)
+			       (if (not (null res))
+				   (return-from limdepthfirstsearch-aux res)))))
+		       (values cutoff?))))))
+    (let ((res (limdepthfirstsearch-aux (make-node :parent nil :state (problem-initial-state problem))
+					problem
+					lim)))
+      (if (eq res :cutoff)
+	  (if cutoff?
+	      :cutoff
+	      nil)
+	  res))))
 				      
 
 ;iterlimdepthfirstsearch
-(defun iterlimdepthfirstsearch (problem)
-  "limited depth first search"
+(defun iterlimdepthfirstsearch (problem &key (lim most-positive-fixnum))
+  "limited depth first search
      st - initial state
      problem - problem information
-     lim - limit of depth iterations
-	(list (make-node :state (problem-initial-state problem))) )
+     lim - limit of depth iterations"
+  (let ((i 0))
+    (loop
+      (let ((res (limdepthfirstsearch problem i :cutoff? T)))
+	(when (and res (not (eq res :cutoff)))
+	  (return res))
+	(incf i)
+	(if (> i lim)
+	    (return nil))))))
+
 	
 ;; Solution of phase 3
 
 
 (defun distance (pos1 pos2)
-  (round (sqrt (+ (expt (- (pos-l pos1) (pos-l pos2)) 2 ) (expt (- (pos-c pos1) (pos-c pos2)) 2 ))))
+  (max (abs (- (pos-l pos2) (pos-l pos1))) (abs (- (pos-c pos2) (pos-c pos1))))
+  ;;(isqrt (+ (expt (- (pos-l pos1) (pos-l pos2)) 2 ) (expt (- (pos-c pos1) (pos-c pos2)) 2 )))
 )
 ;; Heuristic
 (defun compute-heuristic (st)
@@ -117,31 +154,11 @@
         )
       )
     )
-    heuristicDistance ))
-	  
-
-(defun nodesListAux (st parentNode nodesList)
-  (let ((newNode (make-node :state st :parent parentNode )))
-    (if (equal parentNode nil) (setf (node-g newNode) 0) 
-                               (setf (node-g newNode) (+ (state-cost st) (node-g parentNode))))
-    (append nodesList '(newNode))
-
-    (loop for newState in (nextStates st) do 
-      (setf nodesList (nodesListAux newState newNode nodesList))
-    )
-  nodesList 
-  )
-)
-
-(defun createNodesList (st)
-	(let ((nodesList nil))
-			(setf nodesList (nodesListAux st nil nodesList))
-    nodesList
-	)
+    heuristicDistance )
 )
 
 (defun nodeEqual(node1 node2)
-	(eq (node-state node1) (node-state node2))
+	(equalp (state-pos (node-state node1)) (state-pos (node-state node2)))
 )
 
 (defun inList(node lst)
@@ -151,52 +168,53 @@
 	0
 )
 
-(defun addToList (newNode lst)
-	(let ((result nil) (elem nil) (alreadyAdded nil))
-	
-		(loop while (not (NULL lst)) do
-			(setf elem (first lst))
-			(if (and (not( alreadyAdded)) (< (node-f newNode) (node-f elem))) 
-				(progn (setf alreadyAdded t) 
-				(append result '(newNode))))
-			(append result '(elem))
-			(setf lst (rest lst))
-		)
-	)
-)
+(defun insertLst (node lst)
+  "Inserts NODE onto LST, according to FVALUE ordering."
+  (cond ((null lst)(list node))
+        ((<= (node-f node)
+            (node-f (first lst)) )
+         (cons node lst) )
+        (t (cons (first lst)
+                 (insertLst node (rest lst)) )) ) )
 
-(defun addIncompleteNodeList(newNode current lst)
+
+;;deprecated
+(defun addIncompleteNodeList(newNode current lst problem)
 	 (setf (node-parent newNode) current)
      (setf (node-g newNode) (+ (state-cost (node-state newNode)) (node-g current)))
-     (setf (node-h newNode) (compute-heuristic newNode)) ;;FIX ALL compute-heuristics (supoosed to be fn-h)
+     (setf (node-h newNode) (funcall (problem-fn-h problem) (node-state newNode)))
      (setf (node-f newNode) (+ (node-g newNode) (node-h newNode)))
-     (addToList newNode lst)
+     (insertLst newNode lst)
      newNode
 )
 	    
 ;;; A* https://en.wikipedia.org/wiki/A*_search_algorithm
 (defun a* (problem)
-
 	(let ((openSet (list (make-node :state (problem-initial-state problem)
                             :parent nil 
                             :g 0
-                            :h (compute-heuristic (problem-initial-state problem))
-                            :f (compute-heuristic (problem-initial-state problem)))))
+                            :h (funcall (problem-fn-h problem) (problem-initial-state problem))
+                            :f (funcall (problem-fn-h problem) (problem-initial-state problem)))))
         (closedSet nil)
         (current nil)
         (tempNode nil)
         (tentativeG 0))
     (loop while (not (NULL openSet)) do
       (setf current (car openSet))
+
       ;; take the lowest f from openSet
       (if (funcall (problem-fn-isGoal problem) (node-state current)) (return-from a* (reconstructPath current problem)))
       (setf openSet (rest openSet))
-      (addToList current closedSet)
+      (insertLst current closedSet)
       (loop for neighbor in (funcall (problem-fn-nextStates problem) (node-state current)) do	 ;;problem next states
       	(let ((neighborNode (make-node :state neighbor)))
       		(setf tentativeG (+ (state-cost neighbor) (node-g current)))
-      		(setf tempNode (inList neighborNode openSet))
-      		(if (eq 0 tempNode) (setf neighborNode (addIncompleteNodeList neighborNode current openSet)) 
+      		(setf tempNode (inList neighborNode openSet)) 
+      		(if (eq 0 tempNode) (progn 	 (setf (node-parent neighborNode) current)
+									     (setf (node-g neighborNode) (+ (state-cost (node-state neighborNode)) (node-g current)))
+									     (setf (node-h neighborNode) (funcall (problem-fn-h problem) (node-state neighborNode)))
+									     (setf (node-f neighborNode) (+ (node-g neighborNode) (node-h neighborNode)))
+      									 (setf openSet (insertLst neighborNode openSet)))
       							(progn (setf neighborNode tempNode) (if (< tentativeG (node-g neighborNode)) 
       								(progn 
       									(setf (node-parent neighborNode) current)
@@ -208,15 +226,21 @@
       )
     )
   )
-	:failure
+  nil
 )
-  
+
 (defun reconstructPath (current problem)
   (let ((path (list (node-state current))))
-  	(while (not (eq current (problem-initial-state problem)))
+  	(loop while (not (eq (node-state current) (problem-initial-state problem))) do
   		(setf current (node-parent current))
-  		(cons (node-state current) path)
+  		(setf path (cons (node-state current) path))
   	)
+  	;;(print path)
+  	;;(print (format nil "~{~a~^~}" (states-to-list path)))
   	path
   )
+)
+
+(defun best-search (problem)
+	(a* problem)
 )
